a128c6608486b92f1089cfe984785a5405e740a2
Batyrhkan <batirhan250303@icloud.com>
2026-02-14 00:52:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let step = 1;
+
+function next() {
+  document.getElementById("page1").classList.add("hidden");
+  document.getElementById("page2").classList.remove("hidden");
+}
+
+function final() {
+  document.getElementById("page2").classList.add("hidden");
+  document.getElementById("page3").classList.remove("hidden");
+}
